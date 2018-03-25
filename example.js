fa73fa56f9d6b9536c177b550080f13e6ba96086
keeneye7 <keeneye7@gmail.com>
2018-03-25 18:48:23 +0900

    Update example.js

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
       headless: false
   });
   const page = await browser.newPage();
-  await page.goto('http://comic.naver.com/webtoon/detail.nhn?titleId=119874&no=1139&weekday=fri', {waitUntil: 'networkidle'});
+  await page.goto('', {waitUntil: 'networkidle'});
   await page.screenshot({path: 'example.png', fullPage:true});
 //   const firstPage = await page.focus('.wt_viewer');
 //   console.log(firstPage);
